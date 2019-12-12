798d71883156514996ca9312f8b64a8452df75ed
ChillBanana <58822454+ChillBanana@users.noreply.github.com>
2019-12-12 11:19:20 -0600

    Add files via upload

---
+++ character.py
@@ -0,0 +1,199 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Bot to create a character.
+# This program is dedicated to the public domain under the CC0 license.
+"""
+This Bot uses the Updater class to handle the bot.
+First, a few callback functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Creates and stores a character based on the user
+data supplied from chat using a ConversationHandler.
+Send /start to initiate the setup.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
+                          ConversationHandler)
+
+import logging
+import dbprocs
+
+
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
+
+reply_keyboard = [['Brawn', 'Agility'],
+                  ['Intellect', 'Cunning'],
+                  ['Willpower', 'Presence'],
+                  ['Health', 'Name'],
+                  ['Done']]
+
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+
+def facts_to_str(user_data):
+    facts = list()
+
+    for key, value in user_data.items():
+      #Ignore the command stored to show what was set.
+      if key != "character":
+        facts.append('%s - %s' % (key, value))
+
+    return "\n".join(facts).join(['\n', '\n'])
+
+
+def start(bot, update):
+    update.message.reply_text(
+        "Hello! It's time to create your character! Tell me a little about yourself with the choices below.",
+        reply_markup=markup)
+
+    return CHOOSING
+
+
+def regular_choice(bot, update, user_data):
+    text = update.message.text
+    user_data['character'] = text
+    update.message.reply_text('Your %s? Well, hurry and tell me, I don\'t have all day.' % text.lower())
+
+    return TYPING_REPLY
+
+def received_information(bot, update, user_data):
+    text = update.message.text
+    try:
+        category = user_data['character']
+        if category != 'Name':
+            user_data[category] = int(text)
+        else:
+            user_data[category] = text
+        #del user_data['character']
+
+        update.message.reply_text("Current Stats:"
+                                "%s"
+                                "You adjust your values if you'd like, or finish up here."
+                                % facts_to_str(user_data),
+                                reply_markup=markup)
+
+        return CHOOSING
+    except:
+        update.message.reply_text("Please enter in a number instead of text, dingbat.")
+
+
+def done(bot, update, user_data):
+     #Save our data inside the bots DB.
+    error(bot, update, str(update.effective_user.id))
+    dbprocs.insertUpdatePlayer(update.effective_user.id, user_data)
+
+    update.message.reply_text("Alright so here's what you're looking like so far:"
+                              "%s"
+                              "This is what we're going with!" % facts_to_str(user_data))
+    #Since we store in user data, we can always come back and adjust values later.
+    return ConversationHandler.END
+
+def returnStoredCharacter(bot, update, user_data):
+  ourStoredData = dbprocs.retrievePlayer(update.effective_user.id, user_data)
+  if ourStoredData != None:
+    user_data['Brawn'] = ourStoredData[1]
+    user_data['Agility'] = ourStoredData[2]
+    user_data['Intellect'] = ourStoredData[3]
+    user_data['Cunning'] = ourStoredData[4]
+    user_data['Willpower'] = ourStoredData[5]
+    user_data['Presence'] = ourStoredData[6]
+    user_data['Health'] = ourStoredData[7]
+    user_data['Name'] = ourStoredData[8]
+    user_data['character'] = 'Name'
+    bot.sendMessage(update.message.chat_id, facts_to_str(user_data))
+  elif 'Name' in user_data:
+    bot.sendMessage(update.message.chat_id, facts_to_str(user_data))
+  else:
+    bot.sendMessage(update.effective_user.id, "Please create a character before retrieving!")
+
+def changeValues(bot, update, args, user_data):
+  try:
+    storedName = str(args[0])
+    setValue = str(args[1])
+    for key, value in user_data.items():
+      #Ignore the command stored to show what was set.
+      if key == storedName:
+        user_data[key] = setValue
+        dbprocs.insertUpdatePlayer(update.effective_user.id, user_data)
+        bot.sendMessage(update.message.chat_id, "Successfully updated %s to %s" % (storedName, setValue))
+        return
+    bot.sendMessage(update.message.chat_id, "Sorry, %s doesn't exist for a character trait." % storedName)
+  except (IndexError, ValueError, TypeError):
+    update.message.reply_text('Usage: /statSet <stat> <integer value>')
+
+
+def deleteCharacter(bot, update, args, user_data):
+    try:
+        dbprocs.deletePlayer(update.effective_user.id, user_data)
+    except:
+        error(bot, update, "Unable to delete character, something went wrong in the DB or the user doesn't exist.")
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+    
+        
+
+def main():
+
+    #Create the database if it doesn't exist.
+    dbprocs.loadDB()
+    # Create the Updater and pass it your bot's token.
+    updater = Updater("")
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+            CHOOSING: [RegexHandler('^(Brawn|Agility|Intellect|Cunning|Willpower|Presence|Health|Name)$',
+                                    regular_choice,
+                                    pass_user_data=True)
+                       ],
+
+            TYPING_CHOICE: [MessageHandler(Filters.text,
+                                           regular_choice,
+                                           pass_user_data=True),
+                            ],
+
+            TYPING_REPLY: [MessageHandler(Filters.text,
+                                          received_information,
+                                          pass_user_data=True),
+                           ],
+        },
+
+        fallbacks=[RegexHandler('^Done$', done, pass_user_data=True)]
+    )
+    dp.add_handler(conv_handler)
+    #Returns the character.
+    dp.add_handler(CommandHandler("character", returnStoredCharacter, pass_user_data=True))
+    dp.add_handler(CommandHandler("statset", changeValues,  pass_args=True, pass_user_data=True))
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
